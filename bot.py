e6ed41b3c037a4334ce87e177474d4d21c2a4ee3
Anthony Vo <antvo2448@hotmail.com>
2020-06-06 01:25:34 -0400

    Removed some sensitive information (IDs, now obsolete)

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command()
 async def dm(ctx, *, message : str):
-    channel = bot.get_channel(694333537762934865)
+    channel = bot.get_channel("Channel ID here")
     await channel.send(message)
 
-bot.run("NzE4NjgyNTI0NjkxMjAyMTE5.XtsbTQ.R7Wl5h_-KXQYadXQko1vsMYOzWg")
+bot.run("Token ID here")
