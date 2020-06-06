5992e0e829edadc660902aa4dcd17b3f278a43f4
Anthony Vo <antvo2448@hotmail.com>
2020-06-06 01:33:12 -0400

    Added comments for clarification on setting IDs

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command()
 async def dm(ctx, *, message : str):
-    channel = bot.get_channel("Channel ID here")
+    channel = bot.get_channel(ID here) #Channel ID MUST be an int, not a string!
     await channel.send(message)
 
-bot.run("Token ID here")
+bot.run("Token ID here") # Token ID MUST be a string (in quotes)!
