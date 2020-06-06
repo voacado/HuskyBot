3b87d8201b69f661ff9b7f0be325a9219a0d9628
Anthony Vo <antvo2448@hotmail.com>
2020-06-06 19:35:22 -0400

    Fixed issue with only uploading pics/not including text

--- bot.py
+++ bot.py
@@ -22,48 +22,56 @@ bot = commands.Bot(command_prefix='!')
 
 # "!conf" command
 @bot.command()
-async def conf(ctx, *, message : str):
+async def conf(ctx, *, message=""):
 
-    # Obtains User ID and nickname to verify user is in guild/server
-    userID = (ctx.message.author.id)
-    server = bot.get_guild(GUILD_ID)
-    member = server.get_member(userID)
+    # Error check: skips if message has no content or image
+    if (len(message) != 0) or (ctx.message.attachments != []):
 
-    # If member is a member of the server, they will have a name (instead of None)
-    if member != None:
-      # Assigns Discord channel (given channel ID)
-      channel = bot.get_channel(CHANNEL_ID)
+      # Obtains User ID and nickname to verify user is in guild/server
+      userID = (ctx.message.author.id)
+      server = bot.get_guild(GUILD_ID)
+      member = server.get_member(userID)
 
-      # datetime object containing current date and time
-      now = datetime.now()
-      currentTime = now.strftime("%d/%m %H:%M")
+      # If member is a member of the server, they will have a name (instead of None)
+      if member != None:
+        # Assigns Discord channel (given channel ID)
+        channel = bot.get_channel(CHANNEL_ID)
 
-      # Create embed message (looks better)
-      embed = discord.Embed(description=message)
+        # datetime object containing current date and time
+        now = datetime.now()
+        currentTime = now.strftime("%d/%m %H:%M")
 
-      # Set date/time as footer of embed
-      embed.set_footer(text=currentTime)
+        # Create embed message (looks better)
+        embed = discord.Embed(description=message)
 
-      # Image attachments
-      files = []
-      for file in ctx.message.attachments:
-          fp = BytesIO()
-          await file.save(fp)
-          files.append(discord.File(fp, filename=file.filename, spoiler=file.is_spoiler()))
-          imageURL = ctx.message.attachments[0].url
-          embed.set_image(url=imageURL)
-      # The following prints the image as a separate message
-      # await channel.send(files=files)
+        # Set date/time as footer of embed
+        embed.set_footer(text=currentTime)
 
-      # Send embed message to channel
-      await channel.send(embed=embed)
+        # Image attachments
+        files = []
+        for file in ctx.message.attachments:
+            fp = BytesIO()
+            await file.save(fp)
+            files.append(discord.File(fp, filename=file.filename, spoiler=file.is_spoiler()))
+            imageURL = ctx.message.attachments[0].url
+            embed.set_image(url=imageURL)
+        # The following prints the image as a separate message
+        # await channel.send(files=files)
 
-      # Inform user their message has been sent
-      await ctx.send("Your message has been sent!")
+        # Send embed message to channel
+        await channel.send(embed=embed)
 
-    # If member is not a member is the server, they will be tagged None
+        # Inform user their message has been sent
+        await ctx.send("Your message has been sent!")
+
+      # If member is not a member is the server, they will be tagged None
+      else:
+        await ctx.send("You are not a member of the server! The message was not sent.")   
+
+    # If the message contains no image or text
     else:
-      await ctx.send("You are not a member of the server! The message was not sent.")     
+      await ctx.send("Your message does not contain any content. Message failed.")
+
 
 # Runs the bot given bot token ID
 bot.run(BOT_TOKEN)
